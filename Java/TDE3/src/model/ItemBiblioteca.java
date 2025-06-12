2fc33e70c5313dee9c433ad4e59db207768fc7c8
luisscangarelli <luis.scangarelli@soulasalle.com.br>
2025-06-12 19:24:37 -0300

    Update ItemBiblioteca.java

--- Java/TDE3/src/model/ItemBiblioteca.java
+++ Java/TDE3/src/model/ItemBiblioteca.java
@@ -1,1 +1,37 @@
+package model;
 
+public abstract class ItemBiblioteca implements Exibivel {
+    protected String titulo;
+    protected String autor;
+    protected int anoPublicacao;
+    
+    public ItemBiblioteca(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+    public String getAutor() {
+        return autor;
+    }
+    
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+    
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+}
