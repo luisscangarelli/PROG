92b99bb17c721c24aea004dbc54ddd00aba6b94e
luisscangarelli <luis.scangarelli@soulasalle.com.br>
2025-06-12 19:24:52 -0300

    Update Livro.java

--- Java/TDE3/src/model/Livro.java
+++ Java/TDE3/src/model/Livro.java
@@ -1,1 +1,35 @@
+package model;
 
+public class Livro extends ItemBiblioteca {
+    private String editora;
+    private int numeroPaginas;
+    
+    public Livro(String titulo, String autor, int anoPublicacao, String editora, int numeroPaginas) {
+        super(titulo, autor, anoPublicacao);
+        this.editora = editora;
+        this.numeroPaginas = numeroPaginas;
+    }
+    
+    public String getEditora() {
+        return editora;
+    }
+    
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+    
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+    
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+    
+    @Override
+    public void exibirDados() {
+        System.out.println("LIVRO - Título: " + titulo + " | Autor: " + autor + 
+                          " | Ano: " + anoPublicacao + " | Editora: " + editora + 
+                          " | Páginas: " + numeroPaginas);
+    }
+}
